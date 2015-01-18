db0071472ddbb5f59325ce904a27aa567366eae1
Rod Vincent Maitim <rvmaitim@gmail.com>
2015-01-19 00:54:29 +0800

    Sensor: Added Thingspeak functionality, subjected for error correction and bug test

--- sensor/sensorSketch/sensorSketch.ino
+++ sensor/sensorSketch/sensorSketch.ino
@@ -23,6 +23,14 @@ String passphrase = "";
 String telemetryType = "tm";
 String statusType = "st";
 String commandType = "cm";
+
+//TeamSpeak
+byte server[]  = { 184, 106, 153, 149 }; // IP Address for the ThingSpeak API
+String writeAPIKey = "PDK34R7M5G3LYX4Z";    // Write API Key for a ThingSpeak Channel
+const int updateInterval = 30000;        // Time interval in milliseconds to update ThingSpeak   
+Client client(server, 80);
+
+
 void setup()
 {
  powerUp();
@@ -150,4 +158,87 @@ if(getResponse == "OK"{
 }
   
 }
+void sendToThingSpeak()
+{
+ Serial1.println("AT+CGATT?");
+ delay(100);
+ ShowSerialData();
+ Serial1.println("AT+CSTT=\"CMNET\"");//start task and setting the APN,
+ delay(1000);
+ ShowSerialData();
+ Serial1.println("AT+CIICR");//bring up wireless connection
+ delay(300);
+ ShowSerialData();
+ Serial1.println("AT+CIFSR");//get local IP adress
+ delay(2000);
+ ShowSerialData();
+ Serial1.println("AT+CIPSPRT=0");
+ delay(3000);
+ ShowSerialData();
+ Serial1.println("AT+CIPSTART=\"tcp\",\"api.cosm.com\",\"8081\"");//start up the connection
+ delay(2000);
+ ShowSerialData();
+ Serial1.println("AT+CIPSEND");//begin send data to remote server
+ delay(4000);
+ ShowSerialData();
+ String humidity = "1031";//these 4 line code are imitate the real sensor data, because the demo did't add other sensor, so using 4 string variable to replace.
+ String moisture = "1242";//you can replace these four variable to the real sensor data in your project
+ String temperature = "30";//
+ String barometer = "60.56";//
+ Serial1.print("POST /update HTTP/1.1\n");//here is the feed you apply from pachube
+ delay(500);
+ ShowSerialData();
+ Serial1.print("Host: api.thingspeak.com\n");
+ delay(500);
+ ShowSerialData();
+ Serial1.print("Connection: close\n");
+ delay(500);
+ ShowSerialData();
+ Serial1.print("X-THINGSPEAKAPIKEY: "+writeAPIKey+"\n");
+ delay(500);
+ ShowSerialData();
+ Serial1.print("Content-Type: application/x-www-form-urlencoded\n");
+ delay(500);
+ ShowSerialData();
+ Serial1.print("Content-Length: ");
+ delay(500);
+ ShowSerialData();
+ Serial1.println(tsData.length());
+ delay(500);
+ ShowSerialData();
+  Serial1.println("\n\n");
+ delay(500);
+ ShowSerialData();
+  Serial1.println(tsData);
+ delay(500);
+ ShowSerialData();
+ lastConnectionTime = millis();
+    
+    resetCounter = 0;
 
+ 
+ Serial1.println(tsData.length());
+ delay(500);
+ ShowSerialData();
+ Serial1.println("{\"id\": \"02\",\"current_value\": \"" + humidity + "\"},");
+ delay(500);
+ ShowSerialData();
+ Serial1.println("{\"id\": \"03\",\"current_value\": \"" + moisture + "\"},");
+ delay(500);
+ ShowSerialData();
+ Serial1.println("{\"id\": \"04\",\"current_value\": \"" + temperature + "\"}]},\"token\": \"lee\"}");
+ delay(500);
+ ShowSerialData();
+ Serial1.println((char)26);//sending
+ delay(5000);//waitting for reply, important! the time is base on the condition of internet 
+ Serial1.println();
+ ShowSerialData();
+ Serial1.println("AT+CIPCLOSE");//close the connection
+ delay(100);
+ ShowSerialData();
+}
+void ViewSerialResponse()
+{
+ while(Serial1.available()!=0)
+   Serial.write(Serial1.read());
+}
